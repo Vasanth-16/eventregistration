769067eb5fb51126c4d1744c9faec0bcd585bb08
Vasanth-16 <vasanthramalingam742gmail.com>
2024-10-19 16:22:16 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import CreateEvent from './pages/CreateEvent'
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import CreateEvent from './pages/CreateEvent';
+import Example from './pages/Form';
+import tailwindConfig from '../tailwind.config';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <CreateEvent/>
+ 
+    <Example />
   )
 }
 
